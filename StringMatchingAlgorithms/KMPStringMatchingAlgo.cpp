0812ed3a2c59289e31acfcc1ea485017dfa17ead
Prashant Varshney <pv03158@gmail.com>
2020-09-29 16:50:49 +0530

    solved problem

--- StringMatchingAlgorithms/KMPStringMatchingAlgo.cpp
+++ StringMatchingAlgorithms/KMPStringMatchingAlgo.cpp
@@ -7,39 +7,38 @@ main(int argc, char* argv[]) {
     string test, pattern;
     cin >> test >> pattern;
 
-    // 0. FINDING THE LENGTH OF TEST & PATTERN
+    // 0. FINDING THE LENGTH OF TEST & PATTERN STRING
     int n = test.length(), m = pattern.length();
-    // 1. GENERATING PREFIX FUNCTION FOR THE PATTERN
+    // 1. CALCULATING PREFIX FUNCTION ARRAY FOR PATTERN STRING
     vector<int> prefix(m);
     prefix[0] = 0;
-    int i = 1, j = 0;
-    while( i < m ) {
-        if(pattern.at(i) == pattern.at(j)) {
-            prefix[i] = j + 1;
+    int i = 0, j = 1;
+    while(j < m) {
+        if(pattern[i] == pattern[j]) {
+            prefix[j] = i + 1;
             i += 1;
             j += 1;
         }
-        else if(j > 0 )
-            j = prefix[j - 1];
-        else {
-            prefix[i] = 0;
-            i += 1;
-        }
-    } 
+        else if( i > 0)
+            i = prefix[i - 1];
+        else 
+            prefix[j ++ ] = 0;
+    }
 
-    // 2. FINDING THE POSITION WHERE PATTERN EXISTS IN THE TEST STRING
-    i = 0, j = 0;
+    // 2. MATCHING THE GIVEN TEST STRING WITH THE PATTERN STRING
+    i = 0, j = 0;       // USING TWO POINTER APPROACH, I -> TEST & J -> PATTERN
     while( i < n && j < m) {
-        if(test.at(i) == pattern.at(j)) {
+        if(test[i] == pattern[j]) {
             i += 1;
             j += 1;
         }
         else if(j > 0)
             j = prefix[j - 1];
-        else 
+        else
             i += 1;
     }
-
+    // IF PATTERN CONTAINS IN TEST STRING, THEN J == M
     if(j == m)
         cout << i - m << endl;
+
 }
