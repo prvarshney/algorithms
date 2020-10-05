595bd770b3fb1f52e59f6f02cfd38b3039f01578
Prashant Varshney <pv03158@gmail.com>
2020-10-05 23:39:08 +0530

    added prime factorization algorithm with O(sqrt(n)) time complexity

---
+++ Combinatorics/Prime Factorization.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int 
+main(int argc, char* argv[]) {
+    // OPTIMISING IO FROM STDIO
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // TAKING INPUT FROM CONSOLE
+    int num;
+    cin >> num;
+    // PROCESSING OUTPUT
+    int original_number = num;
+    for(int factor = 2 ; factor * factor <= num ; factor++) {
+        int cnt = 0;
+        while(num % factor == 0) {
+            cnt += 1;
+            num /= factor;
+        }
+
+        if(cnt > 0)
+            cout << factor << " x " << cnt << endl;
+    }
+
+    // TO HANDLE PRIME NUMBERS
+    if(num > 1)
+        cout << num << " x " << 1 << endl;
+    return 0;
+}
