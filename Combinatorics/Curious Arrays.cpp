823cd4d571f76b340d563403d88c218a262696d1
Prashant Varshney <pv03158@gmail.com>
2020-10-08 22:35:53 +0530

    solved problem of Curious Arrays (Codeforces)

--- Combinatorics/Curious Arrays.cpp
+++ Combinatorics/Curious Arrays.cpp
@@ -2,66 +2,52 @@
 #include <iostream>
 
 #define mod 1000000007
-#define maxn 100100
+#define maxm 110
 
 using namespace std;
 typedef long long int BigInt;
 
-BigInt power(int a, int x) {
-    if(x == 0) return 1;
-    if(x == 1) return a;
-    BigInt temp = power(a, x / 2);
-    temp = (temp * temp) % mod;
-    if(x & 1 > 0)   // CHECK FOR ODD POWER
-        return (temp * a) % mod;
-    else
-        return temp;
-}
-
-BigInt inverse(BigInt num) {
-    return power(num, mod - 2);
-}
-
-BigInt nCr(int n, int r, BigInt factorial[]) {
-    r = min(r, n - r);
-    BigInt res = factorial[n - r];
-    res = (res * factorial[r]) % mod;
-    res = inverse(res);
-    res = (res * factorial[n]) % mod;
-    return res;
-}
-
 int main(int argc, char* argv[]) {
-    // OPTIMISING IO OPERATIONS FROM STDIO
-    ios::sync_with_stdio(false);
+    // OPTIMISING IO FROM STDIO
+    std::ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     // TAKING INPUT FROM CONSOLE
     int n, m;
     cin >> n >> m;
-    BigInt arr[n + 1];
+    BigInt arr[n];
     for(int idx = 1 ; idx <= n ; idx++)
         cin >> arr[idx];
 
-    // CALCULATING FACTORIALS
-    BigInt factorial[maxn];
-    factorial[0] = 1;
-    for(int idx = 1; idx < maxn; idx++)
-        factorial[idx] = (factorial[idx - 1] * idx) % mod;
-
-    // PERFORMING QUERY OPERATIONS
+    // COMPUTING BINOMIAL COEFFICIENTS USING PASCALS TRIANGLE
+    BigInt dp[n + 200][maxm + 10];
+    for(int i = 0 ; i < n + 200 ; i++) 
+        for(int j = 0 ; j <= min(i, maxm); j++)
+            if(j == 0 || i == j)
+                dp[i][j] = 1;
+            else 
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+            
+    // READING M QUERY FROM CONSOLE
+    BigInt carry[n + 10][maxm + 10];
+    fill_n(&carry[0][0], sizeof(carry) / sizeof(BigInt), 0);
     while(m--) {
         int l, r, k;
         cin >> l >> r >> k;
-        for(int j = l ; j <= r ; j ++) {
-            BigInt carry = nCr(j - l + k, k, factorial);
-            arr[j] = (arr[j] + carry) % mod;
+        for(int iter = 0 ; iter <= k ; iter ++) 
+            carry[l][iter] = (carry[l][iter] + dp[k][k - iter]) % mod;
+        for(int iter = 0 ; iter <= k ; iter ++)
+            carry[r + 1][iter] = (carry[r + 1][iter] - dp[r - l + k + 1][k - iter] + mod) % mod;
+    }
+
+    for(int i = 1; i <= n ; i++) {
+        for(int j  = maxm ; j >= 0 ; j--) {
+            carry[i][j] = (carry[i][j] + carry[i - 1][j]) % mod;
+            carry[i][j] = (carry[i][j] + carry[i - 1][j + 1]) % mod;
         }
     }
 
-    // DISPLAYING ARRAY TO CONSOLE
-    for(int idx = 1 ; idx <= n; idx++)
-        cout << arr[idx] << " ";
-    cout << endl;
+    for(int idx = 1; idx <= n ; idx++)
+        cout << (arr[idx] + carry[idx][0] + mod) % mod << " ";
     return 0;
 }
