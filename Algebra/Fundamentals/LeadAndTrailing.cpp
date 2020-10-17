f6d89f9ec78d2ace1862f177470322e00411f48f
Prashant Varshney <pv03158@gmail.com>
2020-10-17 08:32:49 +0530

    added problem leading and trailing bits (uVa)

--- Algebra/Fundamentals/LeadAndTrailing.cpp
+++ Algebra/Fundamentals/LeadAndTrailing.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
     freopen("inputs.txt", "r", stdin);
     freopen("outputs.txt", "w", stdout);
     // TAKING INPUT FROM CONSOLE
-    int t, n, k;
+    BigInt t, n, k;
     cin >> t;
     while(t --) {
         cin >> n >> k;
@@ -24,17 +24,18 @@ int main(int argc, char* argv[]) {
         double power = k * log10(n);
         int leading = pow(10, power - floor(power)) * 100;
         // CALCULATING n ^ k
-        int trailing = 1;
+        BigInt trailing = 1;
         while(k) {
             if(k & 1 > 0) 
-                trailing *= n % mod;
-            n *= n % mod;
+                trailing = (trailing * n % mod) % mod;
+            n %= mod;
+            n = (n * n) % mod;
             k >>= 1;
         }
         // CALCULATING TRAILING BITS 
-        trailing %= mod;
+        trailing %= 1000;
         // DISPLAYING RESULT
-        cout << leading << "..." << trailing << "\n";
+        printf("%03d...%03d\n", leading, trailing);
     }
     return 0;
 }
